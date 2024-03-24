798246f9e44fdfa9197f4c501b420749c0d0e041
ShinjiShioda <79740804+ShinjiShioda@users.noreply.github.com>
2024-03-24 17:37:34 +0900

    change file name in README.md and LocalDirServer.bat

--- LocalDirServer.bat
+++ LocalDirServer.bat
@@ -1,7 +1,7 @@
-rem fileserver.js launch Batch file
+rem LocalDirServer.js launch Batch file
 rem copyright 2024 Shinji Shioda
 rem node.js execution file set in PATH Environment Value(no full pass)
 @echo off
 title File Server
-rem "C:\Program Files\nodejs\node.exe" %~dp0\fileserver.js
-node.exe %~dp0\fileserver.js %1 %2 %3
+rem "C:\Program Files\nodejs\node.exe" %~dp0\LocalDirServer.js
+node.exe %~dp0\LocalDirServer.js %1 %2 %3
