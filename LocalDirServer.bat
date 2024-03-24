6bd461709527371567b5d335df524aa0f47e0f6c
ShinjiShioda <79740804+ShinjiShioda@users.noreply.github.com>
2024-03-24 17:40:45 +0900

    move @echo off in LocalDirServer.bat

--- LocalDirServer.bat
+++ LocalDirServer.bat
@@ -1,7 +1,7 @@
+@echo off
 rem LocalDirServer.js launch Batch file
 rem copyright 2024 Shinji Shioda
 rem node.js execution file set in PATH Environment Value(no full pass)
-@echo off
 title File Server
 rem "C:\Program Files\nodejs\node.exe" %~dp0\LocalDirServer.js
 node.exe %~dp0\LocalDirServer.js %1 %2 %3
